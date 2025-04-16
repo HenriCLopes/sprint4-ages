eb7247927f8cc248fc3bbcd6a6364cf6f9dc8e1c
HenriCLopes <henriclopes@gmail.com>
2025-04-15 21:45:20 -0300

    CRUD User Pronto

---
+++ dist/user/user.controller.d.ts
@@ -0,0 +1,45 @@
+import { UserService } from "./user.service";
+export declare class UserController {
+    private userService;
+    constructor(userService: UserService);
+    showUsers(): import(".prisma/client").Prisma.PrismaPromise<{
+        id: string;
+        name: string;
+        email: string;
+        password: string;
+        createAt: Date;
+        updadedAt: Date;
+    }[]>;
+    showUser(id: string): import(".prisma/client").Prisma.Prisma__UserClient<{
+        id: string;
+        name: string;
+        email: string;
+        password: string;
+        createAt: Date;
+        updadedAt: Date;
+    } | null, null, import("@prisma/client/runtime/library").DefaultArgs, import(".prisma/client").Prisma.PrismaClientOptions>;
+    createUser(body: any): import(".prisma/client").Prisma.Prisma__UserClient<{
+        id: string;
+        name: string;
+        email: string;
+        password: string;
+        createAt: Date;
+        updadedAt: Date;
+    }, never, import("@prisma/client/runtime/library").DefaultArgs, import(".prisma/client").Prisma.PrismaClientOptions>;
+    updateUser(id: string, body: any): import(".prisma/client").Prisma.Prisma__UserClient<{
+        id: string;
+        name: string;
+        email: string;
+        password: string;
+        createAt: Date;
+        updadedAt: Date;
+    }, never, import("@prisma/client/runtime/library").DefaultArgs, import(".prisma/client").Prisma.PrismaClientOptions>;
+    remove(id: string): import(".prisma/client").Prisma.Prisma__UserClient<{
+        id: string;
+        name: string;
+        email: string;
+        password: string;
+        createAt: Date;
+        updadedAt: Date;
+    }, never, import("@prisma/client/runtime/library").DefaultArgs, import(".prisma/client").Prisma.PrismaClientOptions>;
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { UserService } from "./user.service";
+
+@Controller('user')
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @Get()
+    showUsers() {
+        return this.userService.showUsers()
+    }
+
+    @Get(':id')
+    showUser(@Param('id') id: string) {
+        return this.userService.showUser(id)
+    }
+
+    @Post()
+    createUser(@Body() body) {
+        return this.userService.createUser(body)
+    }
+
+    @Put(':id')
+    updateUser(@Param('id') id: string, @Body() body) {
+        return this.userService.updateUser(id, body)
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.userService.remove(id)
+    }
+}
